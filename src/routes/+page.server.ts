1ef201b43ebcbda371b538b52cecbade39c3bdba
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-17 10:06:50 -0700

    Implement SSR example

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,12 @@
+// We import data from the JSON file. This is for simplicity purposes. In the real app, you would likely fetch this data from a database by making an API call.
+import jobs from "../data/info.json";
+import type { JobsList } from "../types";
+
+const job_list: JobsList = jobs;
+
+// Last, we define a load function that will return an object with the assigned data. SvelteKit will automatically call this function when the page is requested. So, the magic for SSR code happens here as we fetch the data in the server and build the HTML with the data we get back.
+export const load = () => {
+  return {
+    job_list,
+  };
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,24 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import JobDisplay from "../components/JobDisplay.svelte";
+  import type { PageServerData } from "./$types";
+
+  export let data: PageServerData;
+</script>
+
+<div class="home-page">
+  <h1>Job Listing Home page</h1>
+
+  {#each data.job_list as job}
+    <JobDisplay {job} />
+  {/each}
+</div>
+
+<style>
+  .home-page {
+    padding: 2rem 4rem;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+    justify-content: center;
+  }
+</style>
