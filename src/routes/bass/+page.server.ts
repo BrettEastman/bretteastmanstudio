b8d5891b0f109c3afa6757d0fc3f96eefa0e7113
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-09-23 11:42:08 -0700

    feat(ui1): Add ssr function to bass with getList Pocketbase method. Add layout to bass page.

---
+++ src/routes/bass/+page.server.ts
@@ -0,0 +1,27 @@
+import PocketBase from "pocketbase";
+import type { SongList } from "$lib/typesAndInterfaces";
+import { EMAIL, PASSWORD } from "$env/static/private";
+
+const pb = new PocketBase("http://173.230.149.14:80");
+
+export const load = async () => {
+  try {
+    await pb.admins.authWithPassword(EMAIL, PASSWORD);
+    // const records = await pb.collection("songs").getFirstListItem("Bass");
+    const records = await pb.collection("songs").getList(1, 5, {
+      filter: 'instrumentDescription ~ "Bass"',
+    });
+
+    const songList: SongList = records.items.map((record) => ({
+      songTitle: record.songTitle,
+      instrumentDescription: record.instrumentDescription,
+      artistName: record.artistName,
+      songPdfLink: record.songPdfLink,
+    }));
+
+    return { songList };
+  } catch (error) {
+    console.error("Error fetching songs from PocketBase:", error);
+    return { songList: [] };
+  }
+};
