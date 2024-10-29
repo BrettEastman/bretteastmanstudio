68d9646fbe01e70ac306c5d8339a95620186de50
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2024-10-28 22:17:33 -0700

    feat(ui3): Add search functionality to Guitar, Bass, Drums pages.

--- src/routes/drums/+page.svelte
+++ src/routes/drums/+page.svelte
@@ -3,12 +3,24 @@
   import type { PageServerData } from "./$types";
 
   export let data: PageServerData;
+
+  let searchQuery = "";
+
+  $: filteredSongs = data.songList.filter((song) =>
+    song.songTitle.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 </script>
 
 <div class="grid place-items-center gap-4 px-6">
   <h1 class="text-xl text-primary30 font-semibold py-8">Drum songs</h1>
-  {#if data.songList.length > 0}
-    {#each data.songList as song}
+  <input
+    type="text"
+    class="w-1/2 p-2 border border-primary30 dark:border-secondary90 rounded-md"
+    placeholder="Search for a drum song..."
+    bind:value={searchQuery}
+  />
+  {#if filteredSongs.length > 0}
+    {#each filteredSongs as song}
       <SongDisplay {song} />
     {/each}
   {:else}
