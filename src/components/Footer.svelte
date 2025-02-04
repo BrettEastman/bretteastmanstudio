4d08fd593815fe234c80bcb2e60e64d4694e9230
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-02-03 16:51:28 -0800

    feat(Footer and README): Add Icon component with either the github svg or the linkin svg paths, also with props. Update README to note use of Remix icon svgs as well as add badges for Linode and Pocketbase.

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,26 +1,25 @@
+<script>
+  import Icon from "./Icon.svelte";
+</script>
+
 <footer
   class="sticky bottom-0 bg-secondary90 border-t-4 border-solid border-tertiary70 dark:bg-secondary20 dark:border-tertiary70"
 >
-  <div class="flex justify-end gap-4 pr-4 sm:py-6 sm:gap-8">
+  <div
+    class="flex justify-center py-6 mx-auto sm:justify-end gap-4 sm:pr-4 sm:gap-8"
+  >
     <a
       href="https://github.com/BrettEastman/bretteastmanstudio/"
       target="_blank"
       class="text-primary30 dark:text-primary50 hover:text-tertiary60 duration-200"
-      >View source code</a
-    >
-
-    <a
-      href="https://github.com/BrettEastman"
-      target="_blank"
-      class="text-primary30 dark:text-primary50 hover:text-tertiary60 duration-200"
-      >GitHub
+      ><Icon name="github" className="fill-current" size="36" />
     </a>
 
     <a
       href="https://www.linkedin.com/in/brett-austin-eastman/"
       target="_blank"
       class="text-primary30 dark:text-primary50 hover:text-tertiary60 duration-200"
-      >LinkedIn
+      ><Icon name="linkedin" className="fill-current" size="36" />
     </a>
   </div>
 </footer>
