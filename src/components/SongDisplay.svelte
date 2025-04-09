adb08977ed2760fd4a327b0c7ff46311177c9acc
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-04-08 21:34:44 -0700

    refactor: Add one more break point to text size in SongDisplay. On chat page, remove duplicate "mb-4" on p tag, and dd "h-dvh" for dynamic viewport.

--- src/components/SongDisplay.svelte
+++ src/components/SongDisplay.svelte
@@ -13,7 +13,7 @@
   class="border border-primary70 bg-secondary96 dark:bg-secondary20 shadow-customXl dark:shadow-none rounded-xl p-4 w-full md:w-3/4 lg:w-1/2 m-1 active:bg-secondary93 dark:active:bg-secondary10 active:text-white active:shadow-none hover:bg-secondary93 dark:hover:bg-secondary10 hover:text-white hover:shadow-none duration-200"
   target="_blank"
 >
-  <div class="text-sm text-primary10 dark:text-primary90 sm:text-lg">
+  <div class="text-xs text-primary10 dark:text-primary90 sm:text-sm md:text-lg">
     <p>Title: <b class="px-3 sm:px-[14px]">{song.songTitle}</b></p>
     <div class="flex justify-between">
       <span>Artist: <b class="px-1">{song.artistName}</b></span>
