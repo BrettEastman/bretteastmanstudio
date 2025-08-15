2107a235beab3e0a85c491fb380ee7d4ba387a89
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2025-08-15 11:17:24 -0700

    fix(minorTextUpdate): Remove api/env for debugging

--- src/routes/api/env/+server.ts
+++
@@ -1,8 +0,0 @@
-import { json } from "@sveltejs/kit";
-
-export function GET() {
-  return json({
-    PRIVATE_GEMINI_API_KEY: process.env.PRIVATE_GEMINI_API_KEY,
-    all: Object.keys(process.env),
-  });
-}
