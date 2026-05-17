b809e26b5deb6b2f662172e82a008da79c270b58
Brett Austin Eastman <76603041+BrettEastman@users.noreply.github.com>
2026-05-17 00:10:51 -0700

    refactor: update google gemini implementation to use @google/genai, the most modern version google ai api dependency along with its updated syntax

--- src/lib/server/gemini.ts
+++ src/lib/server/gemini.ts
@@ -1,27 +1,23 @@
 import { env } from "$env/dynamic/private";
 import type { PrevMessage } from "$lib/types";
-import { GoogleGenerativeAI } from "@google/generative-ai";
+import { GoogleGenAI } from "@google/genai";
 
-const genAI = new GoogleGenerativeAI(env.PRIVATE_GEMINI_API_KEY);
+const ai = new GoogleGenAI({ apiKey: env.PRIVATE_GEMINI_API_KEY });
 
 export async function getMusicHistorianResponse(
   userMessage: string,
   previousMessages: PrevMessage[] = [],
 ) {
-  const model = genAI.getGenerativeModel({
+  const chat = ai.chats.create({
     model: "gemini-2.5-flash",
-    systemInstruction:
-      'You are a music history chatbot with a long career not dissimilar from Jim DeRogatis or the likes, but you also love classical music and New Music classical. You love to talk about the influences of an artist, i.e. what groups or solo artists or composers influenced them. You like to throw in a recommendation once in a while of an artist you think the user might like, based on their chat history. If a question is not related to music, your response should be, "Now, that I don\'t know. Bring on more music questions though!"',
-  });
-
-  const chat = model.startChat({
-    history: previousMessages,
-    generationConfig: {
+    config: {
+      systemInstruction:
+        'You are a music history chatbot with a long career not dissimilar from Jim DeRogatis or the likes, but you also love classical music and New Music classical. You love to talk about the influences of an artist, i.e. what groups or solo artists or composers influenced them. You like to throw in a recommendation once in a while of an artist you think the user might like, based on their chat history. If a question is not related to music, your response should be, "Now, that I don\'t know. Bring on more music questions though!"',
       maxOutputTokens: 2048,
     },
+    history: previousMessages,
   });
 
-  const result = await chat.sendMessage(userMessage);
-  const response = result.response;
-  return response.text();
+  const response = await chat.sendMessage({ message: userMessage });
+  return response.text;
 }
